4728250c2030a55a72abcd1102287e5d8997b795
Iannis Zannos <zannos@gmail.com>
2023-12-24 12:07:27 +0900

    enable desktop saving

---
+++ postload/zzz_101_tab_keys.el
@@ -0,0 +1,5 @@
+;;; postload/zzz_101_tab_keys.el -*- lexical-binding: t; -*-
+
+(global-set-key (kbd "M-s-<right>") 'tab-next)
+(global-set-key (kbd "M-s-<left>") 'tab-previous)
+(tab-new)
---
+++ postload/zzz_102_enable_desktop_saving.el
@@ -0,0 +1,2 @@
+;;; postload/zzz_102_enable_desktop_saving.el -*- lexical-binding: t; -*-
+(set-default 'desktop-save-mode t)
