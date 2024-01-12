e26d93dd84d82d224cc782c01679178cba3a1c28
Iannis Zannos <zannos@gmail.com>
2024-01-12 17:00:37 +0900

    font height: 160

--- custom.el
+++ custom.el
@@ -10,4 +10,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka")))))
+ '(default ((t (:family "Iosevka" :height 160)))))
